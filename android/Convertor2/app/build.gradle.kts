4fdbe8f41e12cd13a4e6aa7be3a1ecf76c9e0815
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-01-04 09:45:27 +0700

    ,,,

--- android/Convertor2/app/build.gradle.kts
+++ android/Convertor2/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.metricconvert"
+    namespace = "com.example.convertor2"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.metricconvert"
+        applicationId = "com.example.convertor2"
         minSdk = 23
         targetSdk = 34
         versionCode = 1
