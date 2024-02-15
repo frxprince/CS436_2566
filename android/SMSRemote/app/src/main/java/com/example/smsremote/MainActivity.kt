dc0d6efdef5061d561b9e764c353890034ca230f
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-02-15 10:16:36 +0700

    ...

--- android/SMSRemote/app/src/main/java/com/example/smsremote/MainActivity.kt
+++ android/SMSRemote/app/src/main/java/com/example/smsremote/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.smsremote
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+ lateinit var MpAlarm:MediaPlayer
+ lateinit var MPCat:MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +18,19 @@ class MainActivity : AppCompatActivity() {
     btn.setOnClickListener { finish() }
 if (checkSelfPermission(Manifest.permission.RECEIVE_SMS)!=PackageManager.PERMISSION_GRANTED)
  requestPermissions(arrayOf(Manifest.permission.RECEIVE_SMS),1234)
+MpAlarm=MediaPlayer.create(this,R.raw.alarm)
+MPCat=MediaPlayer.create(this,R.raw.cat)
+        if(intent!!.getStringExtra("data")=="cat")
+            MPCat.start()
+        if(intent!!.getStringExtra("data")=="alarm")
+            MpAlarm.start()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+    if(intent!!.getStringExtra("data")=="cat")
+        MPCat.start()
+    if(intent!!.getStringExtra("data")=="alarm")
+        MpAlarm.start()
     }
 }
--- android/SMSRemote/app/src/main/java/com/example/smsremote/MySMSReceiver.kt
+++ android/SMSRemote/app/src/main/java/com/example/smsremote/MySMSReceiver.kt
@@ -11,5 +11,17 @@ class MySMSReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
  var message=Telephony.Sms.Intents.getMessagesFromIntent(intent)[0]
  Log.v("mySMS","From: ${message.displayOriginatingAddress} Msg:${message.displayMessageBody} ")
+  var i=Intent(context,MainActivity::class.java)
+  i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+  i.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+  if (message.displayMessageBody=="cat") {
+      i.putExtra("data","cat")
+      context.startActivity(i)
+      Log.v("mySMS","CAT")
+  }
+        if (message.displayMessageBody=="alarm") {
+            i.putExtra("data","alarm")
+            context.startActivity(i)
+        }
     }
 }
