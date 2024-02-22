4447f0d6d43a266681996355786f72005385f010
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-02-22 11:05:47 +0700

    ...

---
+++ android/Protractor/app/src/main/java/com/example/protractor/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.protractor
+
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(),SensorEventListener {
+ lateinit var seekbar:SeekBar
+ lateinit var txtDegree:TextView
+ lateinit var txtData:TextView
+ lateinit var manager:SensorManager
+ lateinit var sensor:Sensor
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        seekbar=findViewById(R.id.seekBar)
+        txtDegree=findViewById(R.id.tDegree)
+        txtData=findViewById(R.id.tData)
+  manager=getSystemService(SENSOR_SERVICE) as SensorManager
+  sensor=manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) as Sensor
+
+    }
+    override fun onResume() { super.onResume(); manager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL)
+    }
+    override fun onPause() {  super.onPause();manager.unregisterListener(this,sensor)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+     txtData.text=" X:${event!!.values[0]}\nY:${event!!.values[1]} \nZ:${event!!.values[2]}     "
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+}
