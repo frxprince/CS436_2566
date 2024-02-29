3145e47f8fb7f0d92b29db3b7325faee23c40110
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-02-29 11:55:42 +0700

    ..

--- android/myGPS/app/src/main/java/com/example/mygps/MainActivity.kt
+++ android/myGPS/app/src/main/java/com/example/mygps/MainActivity.kt
@@ -5,13 +5,14 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.location.OnNmeaMessageListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import org.w3c.dom.Text
 
-class MainActivity : AppCompatActivity(),LocationListener {
+class MainActivity : AppCompatActivity(),LocationListener,OnNmeaMessageListener {
     lateinit var txtLatLong:TextView
     lateinit var txtNmea:TextView
     lateinit var gps:LocationManager
@@ -26,6 +27,10 @@ if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManage
 requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
     Manifest.permission.ACCESS_COARSE_LOCATION),1234)
 gps=getSystemService(LOCATION_SERVICE) as LocationManager
+gps.addNmeaListener(this)
+    }
+    override fun onNmeaMessage(message: String?, timestamp: Long) {
+txtNmea.text= "$timestamp: $message \n"+ txtNmea.text.toString()
     }
     override fun onResume() {
         super.onResume()
@@ -41,4 +46,6 @@ gps=getSystemService(LOCATION_SERVICE) as LocationManager
    txtLatLong.text="Lat:${location.latitude}\nLong:${location.longitude}\nAlt:${location.altitude
    }\nBearing${location.bearing}\nSpeed:${location.speed}\nClock:${location.time}      "
     }
+
+
 }
