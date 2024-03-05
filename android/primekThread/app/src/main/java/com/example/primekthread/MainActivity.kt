30944541bb4ebfb53e257aa021f5a1076cad41d5
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-03-05 13:45:55 +0700

    ...

---
+++ android/primekThread/app/src/main/java/com/example/primekthread/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.primekthread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnCalculate:Button
+    lateinit var txtInput:EditText
+    lateinit var txtOutput:TextView
+    lateinit var progressbar:ProgressBar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+   btnCalculate=findViewById(R.id.bCalculate)
+   txtInput=findViewById(R.id.eInput)
+   txtOutput=findViewById(R.id.tOutput)
+   progressbar=findViewById(R.id.progressBar)
+   btnCalculate.setOnClickListener {
+     // txtOutput.text= primeK(txtInput.text.toString().toLong()).toString()
+   }
+
+    }
+    fun isPrime(x:Long):Boolean{
+
+        for(i in 2..x-1)
+            if ((x%i) == 0L)
+                return false
+        return true
+    }
+
+    fun primeK(x:Long):Long{
+        var i=3L
+        var c=0L
+        while(true){
+            if(isPrime(i))
+            {
+                c++
+                if(c==x)
+                    return i
+            }
+            i++
+        }
+        return 0
+    }
+}
